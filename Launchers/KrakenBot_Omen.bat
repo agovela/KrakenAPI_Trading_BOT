168af272cc38ee50bd500fd1ec642a2b081c1062
agovela <agovela@yahoo.com>
2025-05-11 23:46:55 -0700

    Add files via upload

---
+++ Launchers/KrakenBot_Omen.bat
@@ -0,0 +1,3 @@
+cd C:\Users\agovela\OneDrive\Projects\Viper\Crypto\Python\Trading 
+C:/"Program Files"/Python311/python.exe KrakenPro_GUI_UI.py
+pause
---
+++ Launchers/KrakenBot_Surface.bat
@@ -0,0 +1,3 @@
+cd C:\Users\blackZun\OneDrive\Projects\Viper\Crypto\Python\Trading
+c:\Python311\Python.exe KrakenPro_GUI_UI.py
+pause
